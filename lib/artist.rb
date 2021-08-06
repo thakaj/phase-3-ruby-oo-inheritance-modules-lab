24729ce4507b8ff7e48a4b726cc174f1a1f33059
thakaj <taulant.hakaj@scranton.edu>
2021-08-06 10:46:22 -0400

    commit tree 5bd30fc5c39dddeb574c7fb20116f5e39091ea1f

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,9 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-
+  extend Memorable
+  extend Findable
+  include Paramable
   @@artists = []
 
   def initialize
@@ -11,22 +13,13 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
+ 
   def add_song(song)
     @songs << song
     song.artist = self
@@ -36,7 +29,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
